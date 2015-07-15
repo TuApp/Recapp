25cd653e9334dd8e41c00ed3b47da6a521d7dc3a
Andres Gutierrez <agutierrezt@unal.edu.co>
2015-07-15 01:07:44 -0500

    Map +Right NavigationDrawer   -We use a editText to add different text to the menu in the navigation drawer   -We use files to store the menuItem that we added dinamically +Map   -We use the google map api to add the map to the tabLayout   -We added a listener which does that when the user click in the title of the marker, this change the activity

--- app/src/main/java/com/unal/tuapp/recapp/MapFragment.java
+++ app/src/main/java/com/unal/tuapp/recapp/MapFragment.java
@@ -1,17 +1,85 @@
 package com.unal.tuapp.recapp;
 
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by andresgutierrez on 7/13/15.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback{
+    private LocationManager handler;
+    private SupportMapFragment mapFragment;
+    private GoogleMap map;
+    private static View view;
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_map,container,false);
+        if(view != null){
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if(parent !=null ){
+                parent.removeView(view);
+            }
+        }
+        try{
+            view = inflater.inflate(R.layout.fragment_map,container,false);
+        }catch (InflateException e){}
+        FragmentManager fm = getChildFragmentManager();
+        mapFragment= (SupportMapFragment) fm.findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return view;
+
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        FragmentManager fm = getChildFragmentManager();
+        mapFragment= (SupportMapFragment) fm.findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (map == null) {
+            map = mapFragment.getMap();
+            map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                @Override
+                public void onInfoWindowClick(Marker marker) {
+                    Intent intent = new Intent(getActivity(),second.class);
+                    startActivity(intent);
+                }
+            });
+
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setAllGesturesEnabled(true);
     }
 }
--- app/src/main/java/com/unal/tuapp/recapp/NavigationDrawer.java
+++ app/src/main/java/com/unal/tuapp/recapp/NavigationDrawer.java
@@ -1,37 +1,48 @@
 package com.unal.tuapp.recapp;
 
-import android.app.Activity;
+
+import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.GravityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.Switch;
+
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.plus.Account;
 import com.google.android.gms.plus.Plus;
 import com.google.android.gms.plus.model.people.Person;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+
 /**
  * Created by andresgutierrez on 7/11/15.
  */
-public class NavigationDrawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
+public class NavigationDrawer extends AppCompatActivity  {
+    private int totalFilter;
     private NavigationView navDrawer;
-    private NavigationView filterNavDrawer;
+    private NavigationView navFilterDrawer;
     private TabLayout tabLayout;
     private DrawerLayout navigationDrawer;
     private Toolbar toolbar;
@@ -39,14 +50,18 @@ public class NavigationDrawer extends AppCompatActivity implements NavigationVie
     private GooglePlus mGooglePlus;
     private static final int PROFILE_PIC_SIZE = 600;
     private View root;
-    private View root1;
+    private ActionBarDrawerToggle drawerToggle;
+    private Button add;
+    private final String TAG = NavigationDrawer.class.getSimpleName();
+    private final String FILE = "filters.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         root = getLayoutInflater().inflate(R.layout.activity_navigation_drawer, null);
         setContentView(root);
-        mGooglePlus = GooglePlus.getInstance(this,null,null);
+        totalFilter = 0;
+        mGooglePlus = GooglePlus.getInstance(this, null, null);
         if(mGooglePlus.mGoogleApiClient.isConnected()){
             Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGooglePlus.mGoogleApiClient);
             Account account = Plus.AccountApi;
@@ -65,6 +80,21 @@ public class NavigationDrawer extends AppCompatActivity implements NavigationVie
 
             new LoadProfileImage(root,imageView).execute(personPhotoUrl);
         }
+        add = (Button) root.findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText) root.findViewById(R.id.filter_text);
+                if(!editText.getText().toString().trim().equals("")){
+                    Menu menu = navFilterDrawer.getMenu();
+                    menu.add(0,totalFilter,Menu.NONE,editText.getText().toString());
+                    totalFilter++;
+                    editText.setText("");
+
+
+                }
+            }
+        });
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
@@ -76,14 +106,84 @@ public class NavigationDrawer extends AppCompatActivity implements NavigationVie
 
         navigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
         navDrawer = (NavigationView) findViewById(R.id.nav_drawer);
-        filterNavDrawer = (NavigationView) findViewById(R.id.filter_nav_drawer);
-        navDrawer.setNavigationItemSelectedListener(this);
+        drawerToggle = new ActionBarDrawerToggle(this,navigationDrawer,toolbar,R.string.drawer_open,R.string.drawer_close){
+            @Override
+            public void onDrawerSlide(View view,float slideOffset){
+                super.onDrawerSlide(view,slideOffset);
+                View temp = getCurrentFocus();
+                if(temp!=null){
+                    InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if(inputManager.isAcceptingText()){
+                        inputManager.hideSoftInputFromWindow(temp.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                        ViewPagerAdapter viewPagerAdapter = (ViewPagerAdapter)viewPager.getAdapter();
+                        Fragment fragment = viewPagerAdapter.getItem(viewPager.getCurrentItem());
+
+                        if(slideOffset==0.0) {
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .detach(fragment)
+                                    .attach(fragment)
+                                    .commit();
+                        }
+                    }
+
+                }
+
+
+
+            }
+        };
+        navigationDrawer.setDrawerListener(drawerToggle);
+        navFilterDrawer = (NavigationView) findViewById(R.id.filter_nav_drawer);
+        navDrawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.favorites:
+                        View view = root.findViewById(R.id.view_pager);
+                        Snackbar.make(view,"algo",Snackbar.LENGTH_LONG).show();
+                        break;
+                    case R.id.appointments:
+                        break;
+                    case R.id.comments:
+                        break;
+                    case R.id.sign_out:
+                        if(mGooglePlus.mGoogleApiClient.isConnected()){
+                            Plus.AccountApi.clearDefaultAccount(mGooglePlus.mGoogleApiClient);
+                            mGooglePlus.mGoogleApiClient.disconnect();
+                            Intent intent = new Intent(NavigationDrawer.this, Recapp.class);
+                            startActivity(intent);
+                        }
+                        break;
+                    case R.id.disconnect:
+                        if(mGooglePlus.mGoogleApiClient.isConnected()){
+                            Plus.AccountApi.clearDefaultAccount(mGooglePlus.mGoogleApiClient);
+                            Plus.AccountApi.revokeAccessAndDisconnect(mGooglePlus.mGoogleApiClient);
+                            mGooglePlus.mGoogleApiClient.disconnect();
+                            Intent intent = new Intent(NavigationDrawer.this, Recapp.class);
+                            startActivity(intent);
+                        }
+                        break;
+                }
+                menuItem.setChecked(true);
+                navigationDrawer.closeDrawers();
+                return false;
+            }
+        });
+        navFilterDrawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                navFilterDrawer.getMenu().removeItem(menuItem.getItemId());
+                return false;
+            }
+        });
+
 
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_recapp, menu);
+        getMenuInflater().inflate(R.menu.menu_navigation_drawer, menu);
         return true;
     }
 
@@ -93,53 +193,49 @@ public class NavigationDrawer extends AppCompatActivity implements NavigationVie
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if(id==android.R.id.home){
-            navigationDrawer.openDrawer(GravityCompat.START);
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            View view = root.findViewById(R.id.view_pager);
+            Snackbar.make(view,"algo",Snackbar.LENGTH_LONG).show();
+        }
+        if(id == R.id.action_navigation){
+            navigationDrawer.openDrawer(navFilterDrawer);
         }
 
         return super.onOptionsItemSelected(item);
     }
     @Override
-    public boolean onNavigationItemSelected(MenuItem menuItem) {
-        switch (menuItem.getItemId()){
-            case R.id.favorites:
-                Snackbar.make(root,"algo",Snackbar.LENGTH_LONG).show();
-                break;
-            case R.id.appointments:
-                break;
-            case R.id.comments:
-                break;
-            case R.id.sign_out:
-                if(mGooglePlus.mGoogleApiClient.isConnected()){
-                    Plus.AccountApi.clearDefaultAccount(mGooglePlus.mGoogleApiClient);
-                    mGooglePlus.mGoogleApiClient.disconnect();
-                    Intent intent = new Intent(NavigationDrawer.this, Recapp.class);
-                    startActivity(intent);
-                }
-                break;
-            case R.id.disconnect:
-                if(mGooglePlus.mGoogleApiClient.isConnected()){
-                    Plus.AccountApi.clearDefaultAccount(mGooglePlus.mGoogleApiClient);
-                    Plus.AccountApi.revokeAccessAndDisconnect(mGooglePlus.mGoogleApiClient);
-                    mGooglePlus.mGoogleApiClient.disconnect();
-                    Intent intent = new Intent(NavigationDrawer.this, Recapp.class);
-                    startActivity(intent);
-                }
-                break;
-        }
-        menuItem.setChecked(true);
-        navigationDrawer.closeDrawers();
-        return false;
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
     }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        saveFilters();
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        loadFilters();
+    }
+
 
     public void setUpToolbar(){
         setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
     public void setUpTabLayout(){
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
@@ -152,11 +248,50 @@ public class NavigationDrawer extends AppCompatActivity implements NavigationVie
     }
     public void setUpViewPager(){
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-        viewPagerAdapter.addFragment(new PlacesFragment(),getResources().getString(R.string.places));
-        viewPagerAdapter.addFragment(new MapFragment(),getResources().getString(R.string.map));
-        viewPagerAdapter.addFragment(new EventsFragment(),getResources().getString(R.string.events));
-        viewPagerAdapter.addFragment(new TutorialFragment(),getResources().getString(R.string.tutorial));
+        viewPagerAdapter.addFragment(new PlacesFragment(), getResources().getString(R.string.places));
+        viewPagerAdapter.addFragment(new MapFragment(), getResources().getString(R.string.map));
+        viewPagerAdapter.addFragment(new EventsFragment(), getResources().getString(R.string.events));
+        viewPagerAdapter.addFragment(new TutorialFragment(), getResources().getString(R.string.tutorial));
         viewPager.setAdapter(viewPagerAdapter);
+    }
+
+    public void saveFilters(){
+        try{
+            FileOutputStream f = this.openFileOutput(FILE,  this.MODE_PRIVATE | this.MODE_APPEND);
+            BufferedWriter output = new BufferedWriter(new PrintWriter(f));
+            Menu menu = navFilterDrawer.getMenu();
+
+            for(int i=0; i<menu.size(); i++){
+                output.write(String.valueOf(menu.getItem(i))+"-"+menu.getItem(i).getItemId());
+                output.newLine();
+            }
+
+            output.close();
+
+        }catch (Exception e){}
+
+    }
+    public void loadFilters(){
+        try{
+            totalFilter = 0;
+            FileInputStream f =  this.openFileInput(FILE);
+            BufferedReader input = new BufferedReader(new InputStreamReader(f));
+            String text;
+            Menu menu = navFilterDrawer.getMenu();
+            menu.clear();
+            while((text=input.readLine())!=null){
+                String values [] = text.split("-");
+                totalFilter++;
+                menu.add(0,Integer.parseInt(values[1]),Menu.NONE,values[0]);
+
+            }
+            input.close();
+            totalFilter--;
+            File dir = this.getFilesDir();
+            File file = new File(dir,FILE);
+            file.delete();
+        }catch (Exception e){}
+
     }
 
 
